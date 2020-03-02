7be2979b4476dcced26fbac796651b523e4e4c0e
ankit27489 <39001919+ankit27489@users.noreply.github.com>
2020-03-02 15:33:47 +0530

    Add files via upload

--- 30_Days_Coding_Challenge/Day_7.py
+++ 30_Days_Coding_Challenge/Day_7.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 23:36:01 2020
+Created on Mon Mar  2 15:32:28 2020
 
 @author: AnkitD
 """
 
-''' Read input from STDIN. Print your output to STDOUT '''
-    #Use input() to read input from STDIN and use print to write your output to STDOUT
 # For this challenge, you are given a range and you need to find 
 # how many prime numbers lying between the given range.
 def main():
@@ -14,24 +12,16 @@ def main():
     a = int(input())
     b = int(input())
 
-    flag = 0
     count = 0
 
-    # Constraints
-    if (a < 1 or a > 100000 or b < 1 or b > 100000):
-        print("Invalid numbers")
-
-    if (a < b):
-        for i in range(a,b+1):
-            flag = 0
-            for divisor in range(2, int(i/2)):
-                if (i/divisor == 0):
-                    print("flag")
-                    flag = 1
-            if (flag == 0):
-                print(i)
+    for num in range(a,b+1):
+        if num > 1:
+            for divisor in range(2, num):
+                if(num%divisor == 0):
+                    break
+            else:
                 count += 1
-    print("Total Prime numbers are: ", count)
+
+    print(count,end="")              
 
 main()
-
